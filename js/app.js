cd5dd4d8c7dbe40b338e2a65abadb5101614170e
Dominika Szukała <dominika.szukala@gmail.com>
2018-09-13 14:46:55 +0200

    Setinterval function added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', function (event) {
+    
+    const secHand = document.querySelector('.hand-second');
+    console.log(secHand)
 
+    setDate = () => {
+        const newDate = new Date()
+
+    }
+    
+    setInterval(setDate, 1000);
+    
 });

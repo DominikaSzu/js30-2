f68ec3d86f396af63cee3e955e9777c869b0f531
Dominika Szukała <dominika.szukala@gmail.com>
2018-09-14 12:15:51 +0200

    clock counting minutes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function (event) {
     
     const secHand = document.querySelector('.hand-second');
+    const minHand = document.querySelector('.hand-minute');
 
 
     setDate = () => {
@@ -9,8 +10,16 @@ document.addEventListener('DOMContentLoaded', function (event) {
 //        Setting seconds hand
         
         const seconds = newDate.getSeconds();
-        const secondsDegrees = ((seconds / 60) * 360);
+        const secondsDegrees = ((seconds / 60) * 360) + 90;
         secHand.style.transform = `rotate(${secondsDegrees}deg)`;
+        
+//        Setting minutes hand
+        
+        const minutes = newDate.getMinutes();
+        const minutesDegrees = ((minutes / 60) * 360) + 90;
+        minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+        
+//        Setting hours hand
 
     }
     

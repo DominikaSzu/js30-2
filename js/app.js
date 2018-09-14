c7713c8f6250b2749eef1d3706f490d129cbe6ee
Dominika Szukała <dominika.szukala@gmail.com>
2018-09-14 12:01:16 +0200

    clock counting seconds

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function (event) {
     
     const secHand = document.querySelector('.hand-second');
-    console.log(secHand)
+
 
     setDate = () => {
-        const newDate = new Date()
+        const newDate = new Date();
+        
+//        Setting seconds hand
+        
+        const seconds = newDate.getSeconds();
+        const secondsDegrees = ((seconds / 60) * 360);
+        secHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     }
     
